e6f66cc6c82a434e56ede8d81bee7c7fb029c6a3
JEISON FONSECA <JEISON FONSECA@LAPTOP-RIB9CLGL>
2022-08-19 22:47:15 -0600

    Add project files.

---
+++ Modelo/Zoologico.cs
@@ -0,0 +1,13 @@
+﻿namespace tareaBase.Modelo
+{
+    public class Zoologico
+    {
+        public int id { get; set; }
+
+        public string? pais { get; set; }
+        public string? nombre { get; set; }
+        public string? telefono { get; set; }
+        public string? sitioWeb { get; set; }
+
+    }
+}
