35950cefb50acf773ba4b02cf4cfa73ddd90c051
FERNANDO FONSECA BRENES JEISON <jeison.fb01@estudiantec.cr>
2022-08-22 19:06:51 -0600

    Se agregaron comentarios

--- Program.cs
+++ Program.cs
@@ -1,18 +1,20 @@
 ﻿using tareaBase.Modelo;
 
-using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore; // is a modern object-database mapper for .NET.
 using Microsoft.Extensions.DependencyInjection;
-using tareaBase.Data;
-var builder = WebApplication.CreateBuilder(args);
+using tareaBase.Data; // access to data
+var builder = WebApplication.CreateBuilder(args); // Initializes a new instance of the WebApplicationBuilder class with preconfigured defaults.
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddDbContext<tareaBaseContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("tareaBaseContext") ?? throw new InvalidOperationException("Connection string 'tareaBaseContext' not found.")));
 
-var app = builder.Build();
+var app = builder.Build(); // is a creational design pattern that allows you to build complex objects step by step.
 
-
+/**
+ * Encargada de inicializar la semilla en la aplicacion web
+ * */
 using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
@@ -27,16 +29,18 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
+app.UseHttpsRedirection(); // it's used to redirect HTTP requests to HTTPS.
+app.UseStaticFiles(); // enables static files to be served:
 
-app.UseRouting();
+app.UseRouting(); // adds route matching to the middleware pipeline.
+                  // This middleware looks at the set of endpoints defined in the app,
+                  // and selects the best match based on the request.
 
-app.UseAuthorization();
+app.UseAuthorization(); // enables authorization capabilities.
 
-app.MapRazorPages();
+app.MapRazorPages(); // allow an ASP.NET Core Web Application to render Razor pages.
 
-app.Run();
+app.Run(); // runs the program
 
 
 
